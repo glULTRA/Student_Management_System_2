520063f5d93d52b755c06e90bbad5bb5b91c79b0
glULTRA <7amakurd2002@gmail.com>
2022-04-15 11:04:18 +0300

    Added student class

--- Student.java
+++ Student.java
@@ -1,3 +1,52 @@
 public class Student {
+    private int Id;
+    private String Fullname;
+    private String Address;
+    private String Mobile;
+    private int Stage;
+    private final String Department = "ISE";
+
+    public Student(){}
+    public Student(int id, String fullname, String address, String mobile, int stage){
+        setId(id);
+        setFullname(fullname);
+        setAddress(address);
+        setMobile(mobile);
+        setStage(stage);
+    }
+
+    public int getId() {
+        return Id;
+    }
+    public void setId(int id) {
+        Id = id;
+    }
+    public String getFullname() {
+        return Fullname;
+    }
+    public void setFullname(String fullname) {
+        Fullname = fullname;
+    }
+    public String getAddress() {
+        return Address;
+    }
+    public void setAddress(String address) {
+        Address = address;
+    }
+    public String getMobile() {
+        return Mobile;
+    }
+    public void setMobile(String mobile) {
+        Mobile = mobile;
+    }
+    public int getStage() {
+        return Stage;
+    }
+    public void setStage(int stage) {
+        Stage = stage;
+    }
+    public String getDepartment() {
+        return Department;
+    }
     
 }
